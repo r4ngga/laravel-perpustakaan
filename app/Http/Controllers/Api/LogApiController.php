1746a54fa77003c70a6add6a22d308e2fcf1552d
r4ngga <ranggajack99@gmail.com>
2023-10-06 22:16:44 +0700

    create log api

---
+++ app/Http/Controllers/Api/LogApiController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Log;
+use App\Room;
+use App\User;
+use Illuminate\Support\Facades\DB;
+use App\Reservation;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use App\Http\Controllers\Controller;
+use Carbon\Carbon;
+
+class LogApiController extends Controller
+{
+    public function getLogs()
+    {
+        
+    }
+}
